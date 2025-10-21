55f5fd4d5f14b8dd119643869add113a2ffef2c1
selenium603 <selenium603@users.noreply.github.com>
2025-10-21 18:37:58 +0800

    UI

--- src/components/GlobalFooter.vue
+++ src/components/GlobalFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-gray-200 text-gray-600 p-4 text-center text-sm select-none mt-16 w-full">
+  <footer class="bg-gray-200 text-gray-600 p-4 text-center text-sm select-none mt-16 w-screen -mx-2 md:-mx-6">
     ©  今天吃什么. 保留所有权利.
   </footer>
 </template>
